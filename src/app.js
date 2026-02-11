08662805a7fdd3d298b30d75c2900ac0be9ce680
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-11 13:47:15 +0700

    init: setup server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { config } from "./config/config.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/health-check", (req, res) => {
+    res.send("API-KEY Backend is working!");
+});
+
+app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`)
+});
