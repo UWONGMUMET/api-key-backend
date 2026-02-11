4d7d29e1895aac4877caa46320f229c0911efc5b
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-02-11 18:16:15 +0700

    feat: initialize express app with middleware and auth routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
 import { config } from "./config/config.js";
+import authRoutes from "./routes/auth.routes.js";
+
+import { globalRateLimit } from "./middlewares/rateLimiter.middleware.js";
+import { requestLogger } from "./middlewares/logger.middleware.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
+
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(globalRateLimit);
+app.use(requestLogger);
+
+app.use("/auth", authRoutes);
+
+app.use(errorHandler);
 
 app.get("/health-check", (req, res) => {
     res.send("API-KEY Backend is working!");
